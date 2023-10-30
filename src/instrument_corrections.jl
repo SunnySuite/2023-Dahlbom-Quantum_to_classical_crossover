b3b2c1ee460cea3e710d5dd2a063d7769b9316a6
ddahlbom <david.dahlbom@gmail.com>
2023-10-30 12:45:16 -0400

    Sequoia

---
+++ src/instrument_corrections.jl
@@ -0,0 +1,26 @@
+σ_from_FWHM(FWHM) = FWHM / (2*√2log(2))
+
+function convolve_sequoia(slice, ωs)
+    @assert length(ωs) == size(slice)[end] "Number of enegy values does not match slice dimension"
+
+    # Load sequoia resolution data
+    data = CSV.read(datadir("sequoia", "FWHM_per_energy.csv"), DataFrame)
+    Es, FWHMs = data.energy, data.FWHM
+    σs = σ_from_FWHM.(FWHMs)
+    println(σs)
+    σ_seq = LinearInterpolation(Es, σs)
+
+    slice′ = zero(slice)
+    for (i, ω) ∈ enumerate(ωs)
+        if ω > 11.3
+            error("No resolution data available above 11.3 meV")
+        end
+        σ = σ_seq[ω]
+        gauss = @. 1/(σ*√(2π))*exp(- (ωs - ω)^2 / (2*σ^2))
+        for j ∈ 1:size(slice)[1]
+            slice′[j,i] = slice[j,:] ⋅ gauss
+        end
+    end
+
+    return slice′
+end
